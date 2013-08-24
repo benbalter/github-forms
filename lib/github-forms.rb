155635c5dc5126fc169e8367f8357334f9e3e7ea
Ben Balter <ben.balter@github.com>
2013-08-24 18:30:44 -0400

    almost working

--- lib/github-forms.rb
+++ lib/github-forms.rb
@@ -2,20 +2,27 @@ require 'octokit'
 require 'sinatra_auth_github'
 require 'redis'
 require 'json'
+require 'securerandom'
+require 'csv'
 
 module GithubForms
   class App < Sinatra::Base
     enable :sessions
 
     set :github_options, {
-      :scopes    => "user, public_repo",
+      :scopes    => "user,public_repo,repo",
       :secret    => ENV['GITHUB_CLIENT_SECRET'],
       :client_id => ENV['GITHUB_CLIENT_ID'],
     }
 
     register Sinatra::Auth::Github
+    use Rack::Session::Cookie, :expire_after => 60*60
 
     TTL = 60*5
+
+    before do
+      session[:id] = SecureRandom.uuid if session_id.nil?
+    end
 
     def redis
       @redis ||= Redis.new \
@@ -30,46 +37,62 @@ module GithubForms
     end
 
     def session_id
-      session['session_id']
+      session[:id]
     end
 
-    def cache(data)
+    def cache_data(data)
       redis.set session_id, data
       redis.expire session_id, TTL
     end
 
-    def retrieve
-      JSON.parse redis.get(session_id)
+    def retrieve_data
+      data = redis.get(session_id)
+      JSON.parse data unless data.nil?
+    end
+
+    def prepare_csv(data)
+      row = CSV::Row.new([],[],false)
+      data.each do |key,value|
+        row << value
+      end
+      row
+    end
+
+    def update_file(current,submission)
+      Base64.encode64 "#{Base64.decode64(current)}\n#{prepare_csv(submission)}"
+    end
+
+    def client
+      token = env['warden'].user.nil? ? "" : env['warden'].user.token
+      Octokit::Client.new :access_token => token
     end
 
     def submit(repo, branch, path, data)
-      client = Octokit::Client.new :access_token => env['warden'].user.token
       file = client.contents( repo, :branch => branch, :path => path )
-      data = data #todo: encode as CSV
-      current = Base64.decode64 file.contents
-      client.update_contents( repo, path, message, file.sha, "#{current}\n{#data}", :branch => branch )
-    end
+      message = "[github forms] update #{path}"
+      content =  update_file file.content, data
+      client.update_contents( repo, path, message, file.sha, content, :branch => branch )
+  end
 
     get '/:owner/:repo/blob/:branch/*' do |owner,repo,branch,path|
-      puts owner
-      puts repo
-      puts branch
-      puts path
-
-      submit "#{owner}/#{repo}", branch, path, "foo"
-
+      data = retrieve_data
+      redirect "/" if data.nil?
+      submit "#{owner}/#{repo}", branch, path, data["data"]
+      redis.del session_id
+      "SUBMITTED"
     end
 
     post '/:owner/:repo/blob/:branch/*' do |owner,repo,branch,path|
+
       unless authenticated?
-         cache({
+        cache_data({
             :owner => owner,
             :repo => repo,
             :branch => branch,
             :path => path,
             :data => request.POST
           }.to_json)
-         authenticate!
+        authenticate!
       end
 
       submit "#{owner}/#{repo}", branch, path, request.POST
